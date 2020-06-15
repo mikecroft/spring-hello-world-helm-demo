e2193f4845464b492204ce2f224e50d71dce6145
Mike Croft <mcroft@redhat.com>
2020-06-15 17:48:57 +0100

    add other envs files and spring active profile env var

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,7 +1,7 @@
 export APP=spring-hello-world-app
 export REG=docker.io/mikecroft
 
-mvn clean package
+mvn clean package -DskipTests
 docker build -t ${APP} .
 docker tag ${APP}:latest ${REG}/${APP}:latest
 docker push ${REG}/${APP}:latest
